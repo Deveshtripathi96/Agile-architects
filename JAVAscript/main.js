c3de17977279011af85d620f673a7d7ca722cd41
Abhishek Yadav <abhi1611919@gmail.com>
2024-03-14 22:23:32 +0530

    testimonial added

--- JAVAscript/main.js
+++ JAVAscript/main.js
@@ -1,4 +1,22 @@
 // change navbar style on scrolling
-window.addEventListener('scroll',()=>{
-    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>300)
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 300);
+});
+
+//show or hide FAQs
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+    //change icon
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
+
